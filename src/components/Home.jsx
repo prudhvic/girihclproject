c01235b9cbe0d603ade6737a227679868b961ea2
prudhvichinnam101@gmail.com <prudhvichinnam101@gmail.com>
2022-11-05 14:13:04 +0530

    initial

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-page">
+      <div className="content">
+        <h2>Welcome To Quiz App</h2>
+        <p>
+          This application is developed for educational purpose, allowing the
+          users to prepare the multiple choice questions for different
+          examinations conducted on provincial and national level
+        </p>
+      </div>
+      <img src="/quizimage.png" />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
